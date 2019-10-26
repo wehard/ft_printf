e89d9478eb71ea56c53994419982ff0a683e0751
Willehard Korander <willehard@gmail.com>
2019-10-26 23:24:21 +0300

    Some improvements.

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/25 14:48:42 by wkorande          #+#    #+#             */
-/*   Updated: 2019/10/25 16:42:44 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/10/26 23:23:07 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,30 @@ static int	ft_countargs(char *str)
 	return (n);
 }
 
+static char ft_remap(int n)
+{
+	if (n >= 0 && n <= 9)
+		return ((char)('0' + n));
+	else
+		return ((char)(n - 10 + 'a'));
+}
+
+static char	*ft_convert_base(unsigned int num, int base)
+{
+	char digits[] = "0123456789abcdef";
+	char buffer[50];
+	char *ptr;
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+	while(num > 0)
+	{
+		*--ptr = digits[num % base];
+		num /= base;
+	}
+	return(ptr);
+}
+
 /*
 ** You have to manage the following conversions: csp
 ** You have to manage the following conversions:
@@ -53,20 +77,54 @@ static int	ft_countargs(char *str)
 
 int			ft_printf(const char *format,  ...)
 {
-	int		size;
-	va_list	valist;
-	int		i;
+	int	i;
+	va_list			valist;
+	char			*str;
+	char			*s;
+	int				len;
 
-	size = ft_countargs((char*)format);
-
+	str = (char*)format;
 	va_start(valist, format);
+
+	len = ft_strlen(format);
 	i = 0;
-	while (i < size)
+	while (i < len)
 	{
-		ft_putnbr(va_arg(valist, int));
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				ft_putchar(va_arg(valist, int));
+			}
+			if (format[i] == 's')
+			{
+				s = va_arg(valist, char *);
+				ft_putstr(s);
+			}
+			if (format[i] == 'p')
+			{
+				int a = va_arg(valist, unsigned int);
+				ft_putstr(ft_convert_base(a, 16));
+			}
+			if (format[i] == 'i')
+			{
+				ft_putnbr(va_arg(valist, int));
+			}
+			if (format[i] == 'x')
+			{
+				int a = va_arg(valist, unsigned int);
+				ft_putstr(ft_convert_base(a, 16));
+			}
+		}
+		else
+		{
+			ft_putchar(format[i]);
+		}
 		i++;
+
 	}
 	va_end(valist);
-	ft_putchar('\n');
+	//ft_putchar('\n');
 	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/25 16:17:13 by wkorande          #+#    #+#             */
-/*   Updated: 2019/10/25 17:12:28 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/10/26 23:23:39 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,15 @@ int	ft_printf(const char *format, ...);
 
 int	main(void)
 {
-	ft_printf("%c%s%pHello", 11, 0, 22);
-	printf("%i%i%iHello", 11, 0, 22);
+	char c = 'a';
+	int *p;
+	int a;
+	a = 55;
+	p = &a;
+	//char *str = "%cWorld";
+	ft_printf("%s World: %x %p\n", "Hello", 15, &p);
+	printf("%s World: %x %p", "Hello", 15, &p);
+	//ft_printf("%x\n", 16);
+	//printf(str, "Hello");
 	return (0);
 }
